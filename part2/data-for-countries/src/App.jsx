285e3dc3c100715a694f2136d3e1d55357bfa5a1
Winn Z <winnwyz@gmail.com>
2025-04-09 11:51:54 +1000

    Implement 2.19 show button

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -4,7 +4,7 @@ import countryService from "./services/node";
 function App() {
   const [countryName, setCountryName] = useState("");
   const [filteredCountries, setFilteredCountries] = useState([]);
-  const [selectedCountry, setSelectedCountry] = useState(null)
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   const changeHandler = async (e) => {
     const searchQuery = e.target.value;
@@ -15,27 +15,46 @@ function App() {
       const filtered = countries.filter((country) =>
         country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
       );
-      setFilteredCountries(filtered);  
-      setSelectedCountry(null);    
+      setFilteredCountries(filtered);
+      setSelectedCountry(null);
     } catch (error) {
       console.error("Error fetching countries:", error);
     }
   };
 
-  useEffect(()=>{
-    const fetchCountryDetails = async() => {
-      if (filteredCountries.length===1) {
-        try {
-          const details = await countryService.getCountryByName(filteredCountries[0].name.common)
-          setSelectedCountry(details)
-        } catch (error) {
-          console.error("Error fetching country details: ", error)
-        }
-      }
+  const fetchCountryDetails = async (countryName) => {
+    try {
+      const details = await countryService.getCountryByName(countryName);
+      setSelectedCountry(details);
+    } catch (error) {
+      console.error("Error fetching country details: ", error);
     }
-    fetchCountryDetails()
-  },[filteredCountries])
+  };
 
+  useEffect(() => {
+    if (filteredCountries.length === 1) {
+      fetchCountryDetails(filteredCountries[0].name.common);
+    }
+  }, [filteredCountries]);
+
+  const displayCountryDetails = () => {
+    return (
+    <div>
+      <h1>{selectedCountry.name.common}</h1>
+      <p>Capital: {selectedCountry.capital}</p>
+      <p>Area: {selectedCountry.area}</p>
+      <h1>Languages</h1>
+      <ul>
+        {Object.entries(selectedCountry.languages).map(([code, name]) => (
+          <li key={code}>{name}</li>
+        ))}
+      </ul>
+      <img
+        src={selectedCountry.flags.png}
+        alt={`Flag of ${selectedCountry.name.common}`}
+      />
+    </div>);
+  };
 
   const renderCountries = () => {
     if (filteredCountries.length > 10) {
@@ -44,31 +63,24 @@ function App() {
       return (
         <ul>
           {filteredCountries.map((country) => (
-            <li key={country.name.common}>{country.name.common}</li>
+            <li key={country.name.common}>
+              {country.name.common}{" "}
+              <button onClick={() => fetchCountryDetails(country.name.common)}>
+                show
+              </button>
+              {selectedCountry &&
+                selectedCountry.name.common === country.name.common &&
+                displayCountryDetails()}
+            </li>
           ))}
         </ul>
       );
     } else if (filteredCountries.length === 1) {
-      return selectedCountry? (
-        <div>
-          <h1>{selectedCountry.name.common}</h1>
-          <p>Capital: {selectedCountry.capital}</p>
-          <p>Area: {selectedCountry.area}</p>
-          <h1>Languages</h1>
-          <ul>
-            {Object.entries(selectedCountry.languages).map(([code, name])=>
-            <li key={code}>
-              {name} 
-            </li>)
-            }
-          </ul>
-          <img src={selectedCountry.flags.png}/>
-        </div>
-      ): (
-        <div>
-          Loading country details...
-        </div>
-      )
+      return selectedCountry ? (
+        displayCountryDetails()
+      ) : (
+        <div>Loading country details...</div>
+      );
     }
     return null;
   };
