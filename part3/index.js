947a05a88782e72fd04af56fdba7c6afd5445de1
winn <winnwyz@gmail.com>
2025-04-11 12:36:01 +1000

    Implement 3.4-3.6

--- part3/index.js
+++ part3/index.js
@@ -48,6 +48,49 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+    const id = request.params.id;
+    const personIndex = data.findIndex((person)=>person.id === id);
+  
+    if (personIndex !== -1) {
+      data.splice(personIndex, 1)
+      response.status(204).end()
+    } else {
+      response.status(404).end();
+    }
+  });
+  
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  if (data.find((person)=>person.name.toLowerCase() === body.name.toLowerCase())) {
+    return response.status(400).json({
+        error: "name already exists",
+    })
+  }
+
+  const generateId = () => {
+    return Math.floor(Math.random() * 1000000).toString();
+  };
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  data.push(person);
+
+  response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
