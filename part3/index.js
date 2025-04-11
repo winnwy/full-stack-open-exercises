cc35e02c40192a973dbe2b0eaf648ccfa3310f51
winn <winnwyz@gmail.com>
2025-04-11 11:56:13 +1000

    Implement 3.1

--- part3/index.js
+++ part3/index.js
@@ -1,13 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (request, response)=>{
-    response.send('<h1>hello</h1>')
+const data = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>hello</h1>");
+});
+
+app.get("/api/persons", (request, response)=> {
+    response.json(data)
 })
 
-const PORT = 3001
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
-})
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
