d9c7aff6ea69e467c8bc3c6d3af9619172dc672f
Winn Z <winnwyz@gmail.com>
2025-04-15 17:40:17 +1000

    Implement 4.2, refractor code into modules

--- part4/index.js
+++ part4/index.js
@@ -1,37 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://winnwyz:123@cluster0.k14cijy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(mongoUrl)
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
